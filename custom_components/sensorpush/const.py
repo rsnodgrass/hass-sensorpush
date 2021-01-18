497ace8740c826a40903cf189b32104a67becc6b
Ryan Snodgrass <rsnodgrass@gmail.com>
2021-01-18 10:08:26 -0800

    added new VPD, Barometric Pressure, Dewpoint sensors; metric/imperial unit config

--- custom_components/sensorpush/const.py
+++ custom_components/sensorpush/const.py
@@ -1,3 +1,5 @@
+from homeassistant.const import TEMP_CELSIUS, TEMP_FAHRENHEIT
+
 SENSORPUSH_DOMAIN = 'sensorpush'
 
 ATTR_BATTERY_VOLTAGE = 'battery_voltage'
@@ -7,18 +9,58 @@ ATTR_OBSERVED_TIME   = 'observed_time'
 CONF_UNIT_SYSTEM = 'unit_system'
 CONF_MAXIMUM_AGE = 'maximum_age' # maximum age (in minutes) of observations before they expire
 
+MEASURE_TEMP = "temperature"
+MEASURE_HUMIDITY = "humidity"
+MEASURE_DEWPOINT = "dewpoint"
+MEASURE_BAROMETRIC_PRESSURE = "barometric_pressure"
+MEASURE_VPD = "vpd"
+
+ICON = 'icon'
+NAME = 'name'
+
+# FIXME: read this from a *localized* JSON configuration map!
+MEASURES = {
+    MEASURE_TEMP: {
+        NAME: "Temperature",
+        ICON: "mdi:thermometer"
+    },
+    MEASURE_HUMIDITY: { # Relative Humidity (Rh %)
+        NAME: "Humidity",
+        ICON: 'mdi:water-percent'
+    },
+    MEASURE_DEWPOINT: {
+        NAME: "Dewpoint",
+        ICON: "mdi:thermometer"
+    },
+    MEASURE_BAROMETRIC_PRESSURE: {
+        NAME: "Barometric Pressure",
+        ICON: "mdi:nature"
+    },
+    MEASURE_VPD: {
+        NAME: "Vapor Pressure Deficit",
+        ICON: "mdi:thermometer"
+    }
+}
+
 UNIT_SYSTEM_IMPERIAL = 'imperial'
 UNIT_SYSTEM_METRIC = 'metric'
 
 UNIT_SYSTEMS = {
     UNIT_SYSTEM_IMPERIAL: { 
-        'system':      'imperial',
-        'temperature': '°F',
-        'humidity':    '%' # 'Rh'
+        'system': 'imperial',
+        MEASURE_BAROMETRIC_PRESSURE: 'inHg',
+        MEASURE_DEWPOINT: TEMP_FAHRENHEIT,
+        MEASURE_HUMIDITY: '%', # 'Rh'
+        MEASURE_TEMP: TEMP_FAHRENHEIT,
+        MEASURE_VPD: 'kPa'
     },
+
     UNIT_SYSTEM_METRIC: { 
-        'system':      'metric',
-        'temperature': '°C',
-        'humidity':    '%' # 'Rh'
+        'system': 'metric',
+        MEASURE_BAROMETRIC_PRESSURE: 'mbar',
+        MEASURE_DEWPOINT: TEMP_CELSIUS, # FIXME: icon mdi:temperature-celsius
+        MEASURE_HUMIDITY: '%', # 'Rh'
+        MEASURE_TEMP: TEMP_CELSIUS,
+        MEASURE_VPD: 'kPa'
     }
 }
--- custom_components/sensorpush/sensor.py
+++ custom_components/sensorpush/sensor.py
@@ -10,9 +10,13 @@ import voluptuous as vol
 from homeassistant.helpers import config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 
-from . import ( SensorPushEntity, SENSORPUSH_SERVICE, SENSORPUSH_SAMPLES,
-                SENSORPUSH_DOMAIN, CONF_MAXIMUM_AGE,
-                CONF_UNIT_SYSTEM, UNIT_SYSTEM_IMPERIAL, UNIT_SYSTEM_METRIC, UNIT_SYSTEMS )
+from . import ( SensorPushEntity, SENSORPUSH_SERVICE, SENSORPUSH_SAMPLES)
+
+from .const import (CONF_UNIT_SYSTEM, UNIT_SYSTEM_IMPERIAL, UNIT_SYSTEM_METRIC, UNIT_SYSTEMS,
+                    CONF_MAXIMUM_AGE, SENSORPUSH_DOMAIN,
+                    UNIT_SYSTEM_IMPERIAL, UNIT_SYSTEM_METRIC, UNIT_SYSTEMS,
+                    MEASURE_TEMP, MEASURE_HUMIDITY, MEASURE_DEWPOINT, MEASURE_BAROMETRIC_PRESSURE,
+                    MEASURE_VPD, MEASURES)
 
 LOG = logging.getLogger(__name__)
 
@@ -20,6 +24,7 @@ DEPENDENCIES = ['sensorpush']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
+        # FIXME: vol.In( UNIT_SYSTEMS.keys() )
         vol.Optional(CONF_UNIT_SYSTEM, default=UNIT_SYSTEM_IMPERIAL): cv.string
     }
 )
@@ -34,12 +39,11 @@ def setup_platform(hass, config, add_entities_callback, discovery_info=None):
         LOG.info("NOT setting up SensorPush -- missing SENSORPUSH_SERVICE")
         return
 
-#    conf = hass.config[SENSORPUSH_DOMAIN]
-    conf = None
+    conf = hass.config[SENSORPUSH_DOMAIN]
 
     unit_system = UNIT_SYSTEM_IMPERIAL
-#    if conf.get(CONF_UNIT_SYSTEM) == UNIT_SYSTEM_METRIC:
-#        unit_system = UNIT_SYSTEM_METRIC
+    if conf.get(CONF_UNIT_SYSTEM) == UNIT_SYSTEM_METRIC:
+        unit_system = UNIT_SYSTEM_METRIC
 
     hass_sensors = []
     for sensor_info in sensorpush_service.sensors.values():
@@ -50,46 +54,36 @@ def setup_platform(hass, config, add_entities_callback, discovery_info=None):
             continue
 
         LOG.info(f"Instantiating SensorPush sensors: {sensor_info}")
-        hass_sensors.append( SensorPushTemperature(hass, conf, sensor_info, unit_system) )
-        hass_sensors.append( SensorPushHumidity(hass, conf, sensor_info, unit_system))
+    
+        # FIXME: ideally measures would only be created IF the sensor supported these (e.g. check "measures" field)
+        for measure in MEASURES:
+            sensor = SensorPushMeasurement(hass, conf, sensor_info, unit_system, measure)
+            hass_sensors.append(sensor)
 
     # execute callback to add new entities
     add_entities_callback(hass_sensors, True)
 
-# pylint: disable=too-many-instance-attributes
-class SensorPushHumidity(SensorPushEntity):
-    """Humidity sensor for a SensorPush device"""
 
-    def __init__(self, hass, config, sensor_info, unit_system):
-        self._state = ''
-        super().__init__(hass, config, 'Humidity', sensor_info, unit_system, 'humidity')
+# pylint: disable=too-many-instance-attributes
+class SensorPushMeasurement(SensorPushEntity):
+    """Measurement sensor for a SensorPush device"""
+    def __init__(self, hass, config, sensor_info, unit_system, measure):
+        self._measure = measure
+        self._unit_system = unit_system
+        self._name = MEASURES[self._measure]['name']
+    
+        self._state = '' # FIXME: should this be Unknown?
+
+        super().__init__(hass, config, self._name, sensor_info, unit_system, measure)
 
     @property
     def icon(self):
-        return 'mdi:water-percent'
+        return MEASURES[self._measure]['icon']
 
     @property
     def unit_of_measurement(self):
-        """Relative Humidity (Rh %)"""
-        return '%'
+        return UNIT_SYSTEMS[self._unit_system][self._measure]
 
     @property
     def unique_id(self):
-        return f"sensorpush_humidity_{self._device_id}"
-    
-# pylint: disable=too-many-instance-attributes
-class SensorPushTemperature(SensorPushEntity):
-    """Temperature sensor for a SensorPush device"""
-
-    def __init__(self, hass, config, sensor_info, unit_system):
-        self._state = ''
-        super().__init__(hass, config, 'Temperature', sensor_info, unit_system, 'temperature')
-
-    @property
-    def unit_of_measurement(self):
-        """Temperature (Fahrenheit or Celsius)"""
-        return UNIT_SYSTEMS.get(self._unit_system).get('temperature')
-
-    @property
-    def unique_id(self):
-        return f"sensorpush_temp_{self._device_id}"
+        return f"sensorpush_{self._measure}_{self._device_id}"
